5ffbbd8b1d35a9a8365e2ead0d01dc5af7b8beab
Yuvadi29 <adityatrivedi612@gmail.com>
2024-07-09 12:14:42 +0530

    JSX Fragments

---
+++ Lecture 12/my-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <> {/*JSX Fragment <></> */}
+      <div>
+        <h1>App</h1>
+        <h2>Hey</h2>
+      </div>
+
+      <div>
+        <h3>Hello World</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
